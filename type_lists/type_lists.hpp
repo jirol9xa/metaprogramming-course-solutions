ac0dfb1a1c2926b86f331066230fb93cd73073f4
Trunov Vladimir <trunov.vv@phystech.edu>
2024-11-03 17:47:34 +0300

    simple ops done

--- type_lists/type_lists.hpp
+++ type_lists/type_lists.hpp
@@ -2,6 +2,7 @@
 
 #include <concepts>
 
+#include <cstddef>
 #include <type_tuples.hpp>
 
 
@@ -22,6 +23,167 @@ concept Empty = std::derived_from<TL, Nil>;
 
 template<class TL>
 concept TypeList = Empty<TL> || TypeSequence<TL>;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <typename H, typename T> requires TypeList<T>
+struct Cons {
+    using Head = H;
+    using Tail = T;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <typename T>
+struct Repeat {
+    using Head = T;
+    using Tail = Repeat<T>;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <TypeList TL>
+struct ToTupleImpl {
+    using type = type_tuples::TupleConcat
+                                        <
+                                            type_tuples::TTuple<typename TL::Head>,
+                                            typename ToTupleImpl<typename TL::Tail>::type
+                                        >::type;
+};
+
+template <>
+struct ToTupleImpl<Nil> {
+    using type = type_tuples::TTuple<>;
+};
+
+template <TypeList TL>
+using ToTuple = ToTupleImpl<TL>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <class T>
+struct FromTupleImpl;
+
+template <class T>
+struct FromTupleImpl<type_tuples::TTuple<T>> {
+    using type = Cons<T, Nil>;
+};
+
+template <class T, class... Ts>
+struct FromTupleImpl<type_tuples::TTuple<T, Ts...>> {
+    using type = Cons<T, typename FromTupleImpl<type_tuples::TTuple<Ts...>>::type>;
+};
+
+template <type_tuples::TypeTuple Tuple>
+using FromTuple = FromTupleImpl<Tuple>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <std::size_t Idx, TypeList TL>
+struct TakeImpl {
+    using type = Cons<typename TL::Head, typename TakeImpl<Idx - 1, typename TL::Tail>::type>;
+};
+
+template <TypeList TL>
+struct TakeImpl<1, TL> {
+    using type = Cons<typename TL::Head, Nil>;
+};
+
+template <TypeList TL>
+struct TakeImpl<0, TL> {
+    using type = Nil;
+};
+
+template <std::size_t Idx>
+struct TakeImpl<Idx, Nil> {
+    using type = Nil;
+};
+
+template <std::size_t N, TypeList TL>
+using Take = TakeImpl<N, TL>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <std::size_t Amnt, TypeList TL>
+struct DropImpl {
+    using type = DropImpl<Amnt - 1, typename TL::Tail>::type;
+};
+
+template <TypeList TL>
+struct DropImpl<0, TL> {
+    using type = TL;
+};
+
+template <std::size_t Amnt>
+struct DropImpl<Amnt, Nil> {
+    using type = Nil;
+};
+
+template <std::size_t N, TypeList TL>
+using Drop = DropImpl<N, TL>::type;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <std::size_t N, typename T>
+using Replicate = Take<N, Repeat<T>>;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <template <typename> class Func, typename T>
+struct Iterate {
+    using Head = T;
+    using Tail = Iterate<Func, Func<T>>;
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <TypeList CurTL, TypeList OrigTL>
+struct CycleImpl {
+    using Head = CurTL::Head;
+    using Tail = CycleImpl<typename CurTL::Tail, OrigTL>;
+};
+
+template <TypeList OrigTL>
+struct CycleImpl<Nil, OrigTL> {
+    using Head = OrigTL::Head;
+    using Tail = CycleImpl<typename OrigTL::Tail, OrigTL>;
+};
+
+template <TypeList TL>
+using Cycle = CycleImpl<TL, TL>;
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+template <template <typename> class Func, TypeList TL>
+struct MapImpl {
+    using type = Cons<Func<typename TL::Head>, typename MapImpl<Func, typename TL::Tail>::type>;
+};
+
+template <template <typename> class Func>
+struct MapImpl<Func, Nil> {
+    using type = Nil;
+};
+
+template <template <typename> class Func, TypeList TL>
+using Map = MapImpl<Func, TL>::type;
 
 // Your fun, fun metaalgorithms :)
 
--- type_lists/type_tuples.hpp
+++ type_lists/type_tuples.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+namespace detail {
+    
+} // namespace detail
 
 namespace type_tuples
 {
@@ -9,5 +12,24 @@ struct TTuple {};
 
 template<class TT>
 concept TypeTuple = requires(TT t) { []<class... Ts>(TTuple<Ts...>){}(t); };
+
+template <class T1, class T2>
+struct TupleConcat;
+
+template <class... Ts1, class... Ts2>
+struct TupleConcat<TTuple<Ts1...>, TTuple<Ts2...>> {
+    using type = TTuple<Ts1..., Ts2...>;
+};
+
+template <class T, class... Ts>
+struct TakeFirstImpl {
+    using type = T;
+    using others = TTuple<Ts...>;
+};
+
+template <typename T>
+struct TakeFirstImpl<T> {
+    using type = T;
+};
 
 } // namespace type_tuples
