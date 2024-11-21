be459423b1d92848dc98d75599dc8d7c7aab516f
Trunov Vladimir <trunov.vv@phystech.edu>
2024-11-21 22:09:40 +0300

    again limit exceed

--- enumerators/EnumeratorTraits.hpp
+++ enumerators/EnumeratorTraits.hpp
@@ -4,8 +4,7 @@
 #include <cstdio>
 #include <string_view>
 #include <type_traits>
-#include <cstdint>
-#include <limits>
+#include <utility>
 
 namespace detail {
     template<typename E>
@@ -25,41 +24,46 @@ struct EnumeratorTraits {
 
     public:
         std::size_t Size = 0;
-        EnumType EnumeratorValues[1025] = {};
-        std::string_view EnumeratorsNames[1025] = {};
+        std::pair<EnumType, std::string_view> Enumerators[1025] = {};
+        std::string_view PrettyStrings[1025] = {};
     
-        template <auto Start, auto End>
-        constexpr inline void for_loop() {
-            if constexpr (Start <= End) {
-                std::string_view pretty = __PRETTY_FUNCTION__;
-                constexpr std::string_view substrToFind = "Start = ";
-                auto name = pretty.substr(pretty.find(substrToFind) + substrToFind.size());
-                name = name.substr(0, name.find_first_of(";"));
-
-                if (name[0] == '(') {
-                    return;
-                }
-
-                if constexpr (!detail::ScopedEnum<Enum>) {
-                    EnumeratorValues[Size] = Start;
-                    EnumeratorsNames[Size] = name;
-                } else {
-                    name = name.substr(name.find_last_of(":"));
-                    EnumeratorValues[Size] = Start;
-                    EnumeratorsNames[Size] = name;
-                }
-
-                ++Size;
-                
-                for_loop<Start + 1, End>();
-            }
+        template <auto Value>
+        constexpr std::string_view GetPretty() {
+            return {__PRETTY_FUNCTION__};
+        }
+    
+        template <auto... Ids>
+        constexpr void CollectPretties(std::integer_sequence<int, Ids...>) {
+            ((PrettyStrings[Ids] = GetPretty<static_cast<MetaEnum>(Ids - MetaMAXN)>()), ...);
         }
 
-        consteval inline void FillMetaImpl() {
+        constexpr void FillMetaImpl() {
             if constexpr (std::is_signed<EnumType>()) {
-                for_loop<static_cast<EnumType>(-MetaMAXN), static_cast<EnumType>(MetaMAXN)>();
+                CollectPretties(std::make_integer_sequence<int, 2 * MetaMAXN + 1>{});
+
+                for (int i = -(int)MetaMAXN; i <= (int) MetaMAXN; ++i) {
+                    auto pretty = PrettyStrings[i + MetaMAXN];
+                    constexpr std::string_view substrToFind = "Value = ";
+                    auto name = pretty.substr(pretty.find(substrToFind) + substrToFind.size());
+                    // for clang
+                    // name = name.substr(0, name.find_first_of("]"));
+
+                    // for gcc
+                    name = name.substr(0, name.find_first_of(";"));
+
+                    if (name[0] == '(') {
+                        continue;
+                    }
+
+                    if constexpr (!detail::ScopedEnum<Enum>) {
+                        Enumerators[Size++] = {i, name};
+                    } else {
+                        name = name.substr(name.find_last_of(":") + 1);
+                        Enumerators[Size++] = {i, name};
+                    }
+                }
             } else {
-                for_loop<0, static_cast<EnumType>(MetaMAXN)>();
+                //for_loop<0, static_cast<EnumType>(MetaMAXN)>();
             }
         }
 
@@ -73,12 +77,12 @@ struct EnumeratorTraits {
     }
     static constexpr Enum at(std::size_t i) noexcept {
         Meta<Enum, MAXN> meta;
-        meta.FillMeta();
+        meta.FillMetaImpl();
         return static_cast<Enum>(meta.Enumerators[i].first);
     }
     static constexpr std::string_view nameAt(std::size_t i) noexcept {
         Meta<Enum, MAXN> meta;
-        meta.FillMeta();
+        meta.FillMetaImpl();
         return meta.Enumerators[i].second;
     }
 };
